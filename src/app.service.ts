b323dd3c2d0055f3a9536e2760b6ac72080ea230
Gayu Yunma Ramadhan <gayuyunma1123@gmail.com>
2025-10-26 09:20:47 +0700

    test ada perubahan ga

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { cpus } from 'os';
+import { hostname } from 'os';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): Record<string, any> {
+    return {
+      status: 'Backend is online!',
+      timestamp: new Date().toISOString(),
+      hostname: hostname(),
+      message: 'Hari Backend API is running smoothly.',
+      version: '0.0.1',
+    };
   }
 }
